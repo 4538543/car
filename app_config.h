e27a887525cdbee6b40ab0c4d5b39a41723e1de9
4538543 <3491979396a@gmail.com>
2026-07-23 20:02:36 +0800

    tune: strengthen end-of-segment deceleration

--- app_config.h
+++ app_config.h
@@ -11,22 +11,22 @@
 #define BLACK_CONFIRM_MS            20U
 #define BLACK_SENSOR_MIN             2U
 #define MISSION_TIMEOUT_MS       20000U
-#define Q1_MIN_DISTANCE_MM        1050U
-#define Q3_DIAGONAL_DISTANCE_MM   1281U
+#define Q1_MIN_DISTANCE_MM        1000U
+#define Q3_DIAGONAL_DISTANCE_MM   1269U
 #define SLOWDOWN_DISTANCE_MM       250U
 #define Q3_STAGE2_REMAINING_MM     300U
 #define Q3_STAGE3_REMAINING_MM     120U
-#define BRAKE_REVERSE_MS            80U
-#define BRAKE_REVERSE_SPEED        220U
+#define BRAKE_REVERSE_MS           100U
+#define BRAKE_REVERSE_SPEED        280U
 #define Q3_POINT_HOLD_TIMEOUT_MS  3000U
 
 /* Encoder is counted on each channel-A rising edge. */
 #define WHEEL_DIAMETER_MM           65U
 #define ENCODER_PULSES_PER_REV    1040U
 #define DRIVE_SPEED                360U
-#define APPROACH_SPEED             210U
-#define Q3_STAGE2_SPEED            290U
-#define Q3_STAGE3_SPEED            210U
+#define APPROACH_SPEED             160U
+#define Q3_STAGE2_SPEED            260U
+#define Q3_STAGE3_SPEED            160U
 #define TURN_SPEED                 260U
 #define TURN_MIN_SPEED             115U
 #define HEADING_KP_NUM               8
