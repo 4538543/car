54bfeb0d7ded9bda155ff3b70e3897f948530eb4
4538543 <3491979396a@gmail.com>
2026-08-01 17:41:37 +0800

    release: add H car V16 gyro gated lap stop

---
+++ 2026电赛H题循迹小车/V16/gyro.h
@@ -0,0 +1,17 @@
+#ifndef GYRO_H
+#define GYRO_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+void Gyro_init(void);
+void Gyro_task1ms(void);
+void Gyro_uartIrqHandler(void);
+
+bool Gyro_hasFreshAngularRate(void);
+int32_t Gyro_getAngularRateCentiDps(void);
+int32_t Gyro_getYawCentiDeg(void);
+int32_t Gyro_getIntegratedAngleCentiDeg(void);
+uint32_t Gyro_getValidFrameCount(void);
+
+#endif
